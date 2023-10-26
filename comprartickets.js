1447eeff6a178c7fcb130a1c41eb1a191cf0d6c7
Lozano Arnaldo <lozanoarnaldo@gmail.com>
2023-10-26 15:54:36 -0300

    Tpintegrador con Javascript

--- comprartickets.js
+++ comprartickets.js
@@ -16,10 +16,20 @@ function calcularpago(){
     }
     
 }
+function seleccioncategoriaet(){
+     document.getElementById('categoria').value = "Estudiante";
+}
+function seleccioncategoriatr(){
+    document.getElementById('categoria').value = "Trainee";
+}
+function seleccioncategoriajr(){
+    document.getElementById('categoria').value = "Junior";
+}
 function borrar(){
     document.getElementById('totalapagar').innerHTML = '';
     document.getElementById('nombre').value = '';
     document.getElementById('apellido').value = '';
     document.getElementById('correo').value = '';
     document.getElementById('cantidaddetickets').value = '';
+    document.getElementById('categoria').value = document.getElementById('selop').value;
 }
