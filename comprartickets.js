3f76aa098ec0832e2f87243ece1a08ed58ec743d
Lozano Arnaldo <lozanoarnaldo@gmail.com>
2023-10-28 15:33:43 -0300

    TPintegrador con Js

--- comprartickets.js
+++ comprartickets.js
@@ -1,20 +1,8 @@
 function calcularpago(){
-    let cantidad=Number(document.getElementById('cantidaddetickets').value);
-    let categoria=document.getElementById('categoria').value;
-    let pagoinicial = cantidad * 200;
-    if(categoria == "Estudiante" ) {
-        let pagototal = Number(pagoinicial * 0.2);
-        document.getElementById('totalapagar').innerHTML = pagototal;
-    }
-    if(categoria == "Trainee" ) {
-        let pagototal = Number(pagoinicial * 0.5);
-        document.getElementById('totalapagar').innerHTML = pagototal;
-    }
-    if(categoria == "Junior" ) {
-        let pagototal = Number(pagoinicial * 0.85);
-        document.getElementById('totalapagar').innerHTML = pagototal;
-    }
-    
+    verificardatos();
+
+     total();
+
 }
 function seleccioncategoriaet(){
      document.getElementById('categoria').value = "Estudiante";
@@ -32,4 +20,46 @@ function borrar(){
     document.getElementById('correo').value = '';
     document.getElementById('cantidaddetickets').value = '';
     document.getElementById('categoria').value = document.getElementById('selop').value;
+}
+
+function verificardatos(){
+    let nombre = document.getElementById('nombre').value;
+    let apellido = document.getElementById('apellido').value;
+    let correo = document.getElementById('correo').value;
+    let cantidad=Number(document.getElementById('cantidaddetickets').value);
+    let categoria=document.getElementById('categoria').value;
+    if( nombre == ''){
+       alert("Debe completar el nombre");
+    }
+    if(apellido == ''){
+        alert("Debe completar el apellido");
+     }
+     if(correo == ''){
+        alert("Debe completar el correo");
+     }
+     if(cantidad == 0 || cantidad > 100){
+        alert("Debe ingresar cantidad de tickets entre 1 y 100");
+     }  
+     if(categoria == "Selecciona una opción"){
+        alert ("Debe seleccionar una categoria");
+     }
+
+     
+}
+function total(){
+    let cantidad=Number(document.getElementById('cantidaddetickets').value);
+        let categoria=document.getElementById('categoria').value;
+    let pagoinicial = cantidad * 200;
+    if(categoria == "Estudiante" ) {
+        let pagototal = Number(pagoinicial * 0.2);
+        document.getElementById('totalapagar').innerHTML = pagototal;
+    }
+    if(categoria == "Trainee" ) {
+        let pagototal = Number(pagoinicial * 0.5);
+        document.getElementById('totalapagar').innerHTML = pagototal;
+    }
+    if(categoria == "Junior" ) {
+        let pagototal = Number(pagoinicial * 0.85);
+        document.getElementById('totalapagar').innerHTML = pagototal;
+    }
 }
